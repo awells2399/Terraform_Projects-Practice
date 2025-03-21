36f02fd05c22d1f4970f733b6df84cfa72a546f5
awells2399 <alwells239@gmail.com>
2025-03-21 16:06:36 -0400

    project iam and users

---
+++ proj-iam-users/data.tf
@@ -0,0 +1,8 @@
+data "aws_iam_policy" "managed_policies" {
+  for_each = toset(local.role_policies_list[*].policy)
+  arn      = "arn:aws:iam::aws:policy/${each.value}"
+}
+
+data "aws_caller_identity" "current" {
+
+}
